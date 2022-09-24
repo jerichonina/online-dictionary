dfd7c70b9ae93c7461231ae15af46b8113f303ad
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-09-24 19:02:45 +0800

    added space between audio element and phonetics

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,7 @@ export default function Phonetic(props) {
     console.log(props.phonetic);
     return (
       <div className="phonetics">
-        <audio controls src={props.phonetic.audio}></audio>
+        <audio controls src={props.phonetic.audio}></audio> {" "}
         <span>{props.phonetic.text}</span>
       </div>
     );
