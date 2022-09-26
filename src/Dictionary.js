f58fe9ef7121f289b9198bbebd6b88b9deaa5468
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-09-26 21:40:22 +0800

    added a photo component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos,setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
@@ -15,6 +17,7 @@ export default function Dictionary(props) {
 
   function handlePexelsResponse(response) {
     console.log(response.data);
+    setPhotos(response.data.photos);
   }
 
   function search() {
@@ -53,6 +56,7 @@ export default function Dictionary(props) {
           </form>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
