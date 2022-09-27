eef35fb39aae0fa9458a83f2dcadaadd71ba9f3c
Jerichonina <102739349+jerichonina@users.noreply.github.com>
2022-09-27 23:41:44 +0800

    updated phonetics component

--- src/Results.js
+++ src/Results.js
@@ -9,7 +9,9 @@ export default function Results(props) {
       <div className="Results">
         <section>
           <h2>{props.results.word}</h2>
-          {props.results.phonetics.map(function (phonetic, index) {
+          {props.results.phonetics.filter(function(phonetic){
+            return phonetic.audio;
+          }).map(function (phonetic, index) {
             return (
             <div key={index}>
               <Phonetic phonetic={phonetic} />
